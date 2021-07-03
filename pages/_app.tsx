01277995f8de256a622e88ce32df259a19966bd6
Felix <felix.zhou@mail.utoronto.ca>
2021-07-02 22:45:32 -0400

    set up focus-visible

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import 'focus-visible/dist/focus-visible';
+import { Global, css } from '@emotion/react';
+
+const GlobalStyles = css`
+  .js-focus-visible :focus:not([data-focus-visible-added]) {
+    outline: none;
+    box-shadow: none;
+  }
+`;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <Global styles={GlobalStyles} />
       <Component {...pageProps} />
     </ChakraProvider>
   );
