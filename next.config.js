c157f9f78fe6a63830f07e26f66278885af1c566
Felix <felix.zhou@mail.utoronto.ca>
2021-07-07 02:14:34 -0400

    configure cors

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://0.0.0.0:3000/:path*'
+      }
+    ]
+  },
+};
